6c97e2ad4d448b5e05fa890c6a3e5f1b2c02b4a8
Nicholas Kostelnik <nkostelnik@gmail.com>
2014-04-29 17:04:14 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+var http = require('http');
+var qs = require('querystring');
+var request = require('request');
+
+function sendToRouter(command) {
+  request.post("http://localhost:8081", {body: JSON.stringify(command)}, function (err, response, body) {
+    if (err) {
+      console.error("Unable to reach router")
+      return console.error(err);
+    }
+  })
+}
+
+function detectCommand(command) {
+  var options = {
+    url: 'https://api.wit.ai/message?q=' + qs.escape(command.text),
+    headers: {"Authorization": "Bearer UE47AC55EFCKJM2WQCIDFBT5F3RK5EGW" }
+  }
+
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var message = JSON.parse(body)
+      var resultData = JSON.stringify({result: body})
+      if (command.callback != undefined) {
+        request.post(command.callback, {body: resultData})
+      }
+      console.log(body)
+      var routeCommand = {"category": message.outcome.intent, "commands": message.outcome.entities}
+      sendToRouter(routeCommand)
+    }
+  })
+}
+
+function createServer(port) {
+  http.createServer(function (req, res) {
+    var body = "";
+
+    req.on('data', function (chunk) {
+      body += chunk;
+    })
+
+    req.on('end', function () {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.end('OK!');
+
+      console.log(body);
+
+      var formData = JSON.parse(body);
+      detectCommand(formData)
+    });
+  }).listen(port)
+}
+
+function start() {
+  createServer(8080)
+}
+
+start()
